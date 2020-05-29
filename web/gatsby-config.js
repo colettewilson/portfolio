36acf419a16b0fad6223f773be0ca50ca42e250b
Colette Wilson Wyatt <colette.wilson-wyatt@ltgplc.com>
2020-05-29 19:29:25 +0100

    update config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -7,13 +7,14 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    siteUrl: `${process.env.GATSBY_SITE_URL}`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-react-helmet-canonical-urls`,
       options: {
-        siteUrl: ``,
+        siteUrl: `${process.env.GATSBY_SITE_URL}`,
       },
     },
     {
