0bcde144485d742efdcdef81b559313bc6181a34
Colette Wilson Wyatt <colette.wilson-wyatt@ltgplc.com>
2020-05-30 09:45:09 +0100

    form changes

--- web/src/components/contactForm.js
+++ web/src/components/contactForm.js
@@ -1,21 +1,24 @@
-import React from "react"
+import React, { useState, useRef } from "react"
 
 import styles from "./contactForm.module.scss"
 
 const ContactForm = () => {
+  const [state, setState] = useState({})
+  const contactForm = useRef(null)
+
   const encode = (data) => {
     return Object.keys(data)
       .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
       .join('&')
   }
 
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value })
+  const handleChange = (evt) => {
+    setState({ ...state, [evt.target.name]: evt.target.value })
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    const form = evt.target
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -24,27 +27,42 @@ const ContactForm = () => {
         ...state,
       }),
     })
+    .then(() => {
+      contactForm.current.setAttribute("submitted", true)
+      contactForm.current.reset()
+    })
     .catch((error) => alert(error))
   }
 
   return (
-    <form className={styles.contactForm} name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field">
-      <input type="hidden" name="form-name" value="contact" />
-      <div className={styles.formField}>
-        <label htmlFor="name">Name:</label>
-        <input id="name" name="name" type="text" />
+    <form className={styles.contactForm} ref={contactForm} name="contact" method="post" action="/" data-netlify="true" netlify-honeypot="bot-field" onSubmit={handleSubmit}>
+      <div className={styles.formFront}>
+        <input type="hidden" name="form-name" value="contact" />
+        <input name="bot-field" onChange={handleChange} hidden />
+        <div className={styles.formField}>
+          <label htmlFor="name">Name:</label>
+          <input id="name" name="name" type="text" required />
+        </div>
+        <div className={styles.formField}>
+          <label htmlFor="email">Email:</label>
+          <input id="email" name="email" type="email" required />
+        </div>
+        <div className={styles.formField}>
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" cols="30" rows="10" required></textarea>
+        </div>
+        <div className={styles.formButton}>
+          <button type="submit">Send</button>
+        </div>
       </div>
-      <div className={styles.formField}>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" />
-      </div>
-      <div className={styles.formField}>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" cols="30" rows="10"></textarea>
-      </div>
-      <input name="bot-field" onChange={handleChange} hidden />
-      <div className={styles.formButton}>
-        <button type="submit">Send</button>
+      <div className={styles.formMessage}>
+        <div className={styles.formSuccess}>
+          <div className={styles.formSuccessIcon}>
+            <svg><circle cx="35" cy="35" r="30" /></svg>
+          </div>
+          <p>Your message was sent.</p>
+        </div>
+        <p>Thank you for contacting me, I'll get back to you as soon as I can.</p>
       </div>
     </form>
   )
