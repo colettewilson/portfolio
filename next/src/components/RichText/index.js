be4304f7208e644a8a3ad5a5edd824d87ee60588
Colette Wilson Wyatt <colette@under2.agency>
2021-06-06 13:31:33 +0100

    fix portable text links

--- next/src/components/RichText/index.js
+++ next/src/components/RichText/index.js
@@ -1,5 +1,8 @@
 import BaseBlockContent from "@sanity/block-content-to-react"
 import React from "react"
+import Link from "next/link"
+
+import { LinkTest } from "../../helpers/link"
 
 import styles from "./richText.module.scss"
 
@@ -11,6 +14,19 @@ const serializers = {
         default:
           return BaseBlockContent.defaultSerializers.types.block(props)
       }
+    }
+  },
+  marks: {
+    link: ({children, mark}) => {
+      const linkType = LinkTest(mark.link)
+      const target = mark.newTab ? {target: "_blank"} : null
+      const rel = mark.noFollow ? {rel: "nofollow noopener noreferrer"} : null
+      
+      return linkType === "internal" ? (
+        <Link href={mark.link}><a>{children}</a></Link>
+      ) : (
+        <a href={mark.link} {...target} {...rel}>{children}</a>
+      )
     }
   }
 }
