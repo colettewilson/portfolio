2fd2bf3d77e3cab46d497537a05eaacd2364cc22
Colette Wilson Wyatt <colette@under2.agency>
2021-06-08 20:02:55 +0100

    get ids right on accordion

--- next/src/components/Accordion/Tab.js
+++ next/src/components/Accordion/Tab.js
@@ -7,7 +7,7 @@ import RichText from '../RichText'
 
 import styles from './tab.module.scss'
 
-const Tab = ({ name, position, start, end, responsibilities, skills }) => {
+const Tab = ({ _id, name, position, start, end, responsibilities, skills }) => {
   const [expanded, setExpanded] = useState(false)
   const [height, setHeight] = useState(0)
   const content = useRef()
@@ -23,27 +23,27 @@ const Tab = ({ name, position, start, end, responsibilities, skills }) => {
   return (
     <div className={styles.tab}>
       <input
-        id={`${name}_${position}_input`}
+        id={`${_id}_input`}
         className={styles.tabInput}
         type="checkbox"
         name={`${name}_${position}`}
       />
       <label 
-        id={`${name}_${position}`}
-        htmlFor={`${name}_${position}`}
+        id={_id}
+        htmlFor={`${_id}_input`}
         className={styles.tabLabel}
         aria-expanded={expanded}
-        aria-controls={`${name}_${position}_content`}
+        aria-controls={`${_id}_content`}
         onClick={() => setExpanded(!expanded)}
       >
         <span>{name} - <em>{position}</em></span>
         <span className={styles.tabLabelDate}>{formatDate(start)} - {end ? formatDate(end) : 'present'}</span>
       </label>
       <div
-        id={`${name}_${position}_content`}
+        id={`${_id}_content`}
         className={styles.tabContent}
         role="region"
-        aria-labelledby={`${name}_${position}`}
+        aria-labelledby={_id}
         aria-hidden={!expanded}
         ref={content}
         style={{ maxHeight: `${height}px` }}
