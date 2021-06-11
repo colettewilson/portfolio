0cd96db7609e5212ee186d034adae4dcc2dbacbc
Colette Wilson Wyatt <colette@under2.agency>
2021-06-11 07:54:35 +0100

    update nav links to next link

--- next/src/components/Nav/index.js
+++ next/src/components/Nav/index.js
@@ -12,19 +12,27 @@ const Nav = (props) => {
       <nav className={styles.nav} id="menu">
         <ul>
           <li>
-            <Link href="/#about" onClick={props.handleMenuReset}>About</Link>
+            <Link href="/#about">
+              <a onClick={props.handleMenuReset}>About</a>
+            </Link>
           </li>
           <li>
-            <Link href="/#experience" onClick={props.handleMenuReset}>Experience</Link>
+            <Link href="/#experience">
+              <a onClick={props.handleMenuReset}>Experience</a>
+            </Link>
           </li>
           <li>
-            <Link href="/#portfolio" onClick={props.handleMenuReset}>Portfolio</Link>
+            <Link href="/#portfolio">
+              <a onClick={props.handleMenuReset}>Portfolio</a>  
+            </Link>
           </li>
           <li>
             <a href="/colette-wilson-wyatt-cv.pdf" onClick={props.handleMenuReset} target="_blank" rel="nofollow noopener noreferrer">Resume</a>
           </li>
           <li>
-            <Link href="/#contact" onClick={props.handleMenuReset}>Contact</Link>
+            <Link href="/#contact">
+              <a onClick={props.handleMenuReset}>Contact</a>
+            </Link>
           </li>
         </ul>
         <ModeToggle handleMode={props.handleMode} mode={props.mode} />
